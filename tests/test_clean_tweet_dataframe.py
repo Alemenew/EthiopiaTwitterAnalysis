70c9f5ed531ac53b9d3c42e9c7bbf4525c4d918f
Alex <alex@Alexs-MacBook-Pro.local>
2021-11-06 09:36:52 +0300

    gitaction

---
+++ tests/test_clean_tweet_dataframe.py
@@ -0,0 +1,8 @@
+import unittest
+import pandas as pd
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join("/Users/alex/Desktop/10_acadamy/Twitter-Data-Analysis")))
+
+from clean_tweets_dataframe import read_csv
+from clean_tweets_dataframe import CleanTweets
