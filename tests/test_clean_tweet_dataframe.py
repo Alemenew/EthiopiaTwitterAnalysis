846df9868a474b07f0dd544f6379108f6d8c971f
alemenew <alemenewendawek@gmail.com>
2021-11-07 14:31:42 +0300

    github action

--- tests/test_clean_tweet_dataframe.py
+++ tests/test_clean_tweet_dataframe.py
@@ -1,8 +1,17 @@
 import unittest
 import pandas as pd
 import sys
-import os
-sys.path.append(os.path.abspath(os.path.join("/Users/alex/Desktop/10_acadamy/Twitter-Data-Analysis")))
 
-from clean_tweets_dataframe import read_csv
+sys.path.insert(1, '/Users/alex/Desktop/10_acadamy/Twitter-Data-Analysis')
+from extract_dataframe import read_json
+from extract_dataframe import TweetDfExtractor
+from clean_tweets_dataframe import df
 from clean_tweets_dataframe import CleanTweets
+
+class TestTweetDFClean(unittest.TestCase):
+    def setUp(self) -> pd.DataFrame:
+        #self.CleanTweets
+        self.df = TweetDfExtractor(tweet_list[:5])
+
+if __name__ == '__main__':
+	unittest.main()
